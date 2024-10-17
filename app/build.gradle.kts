c47e2facd27b4d9d7a6d67e8822210febcb08941
Ashok <ashok06082@gmail.com>
2024-10-17 16:55:27 +0530

    Adds a Password Text feild for signIn & signUp screens

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
+    implementation (libs.androidx.material.icons.extended)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.ui.text.google.fonts)
