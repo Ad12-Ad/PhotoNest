90a4f010fd3b54f0ea881ccad7f8a38a5323ec3f
Ashok <ashok06082@gmail.com>
2024-10-18 20:21:27 +0530

    Adds Ui of the SignUp screen

--- app/src/main/java/com/example/photonest/ui/screens/signup/SignUpScreen.kt
+++ app/src/main/java/com/example/photonest/ui/screens/signup/SignUpScreen.kt
@@ -1,6 +1,5 @@
-package com.example.photonest.ui.screens.singup
+package com.example.photonest.ui.screens.signup
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,21 +8,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.photonest.R
 import com.example.photonest.ui.components.AnnotatedText
 import com.example.photonest.ui.components.BackTxtBtn
@@ -34,30 +26,19 @@ import com.example.photonest.ui.components.OnBoardingTextField
 import com.example.photonest.ui.components.OnboardingCircleBtn
 import com.example.photonest.ui.components.ShowHidePasswordTextField
 import com.example.photonest.ui.components.SignSocialButtons
-import com.example.photonest.ui.components.SocialButton
-import com.example.photonest.ui.components.annotatedText
+import com.example.photonest.ui.theme.PhotoNestTheme
 
 @Composable
 fun SignUpScreen(
-    viewModel: SignUpViewModel = viewModel(),
-    onSignInSuccess: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
-    val uiState by viewModel.uiState.collectAsState()
-
-    SignInContent(
-//        uiState = uiState,
-        modifier = Modifier.padding(horizontal = 16.dp)
+    SignUpContent(
+        modifier = modifier
     )
 }
 
 @Composable
-fun SignInContent(
-//    uiState: SignUpUiState,
-//    onEmailChange: (String) -> Unit,
-//    onPasswordChange: (String) -> Unit,
-//    onSignInClick: () -> Unit,
-//    onSignInSuccess: () -> Unit,
+fun SignUpContent(
     modifier: Modifier = Modifier
 ) {
     Column(
@@ -70,14 +51,13 @@ fun SignInContent(
             horizontalArrangement = Arrangement.SpaceBetween,
             modifier = Modifier.fillMaxWidth()
         ){
-            Heading1(text = "Create an \naccount")
+            Heading1(text = "Create an \naccount", fontColor = MaterialTheme.colorScheme.primary)
             BackTxtBtn(modifier = Modifier.padding(top = 12.dp)) {}
         }
         OnBoardingTextField(
             value = "",
             onValueChange = {},
             label = "Username or Email",
-            focusRequester = FocusRequester(),
             prefix = {
                 Icon(
                     painter = painterResource(id = R.drawable.user_logo),
@@ -90,12 +70,11 @@ fun SignInContent(
             value = "",
             onValueChange = {},
             label = "Name",
-            focusRequester = FocusRequester()
         )
         ShowHidePasswordTextField(label = "Password")
         ShowHidePasswordTextField(label = "Confirm Password")
         AnnotatedText(
-            text1 = "If already have an account", text2 = "Sign In",
+            text1 = "If already have an account.", text2 = "Sign In",
             modifier = Modifier.height(24.dp)
         )
         Row (
@@ -103,7 +82,7 @@ fun SignInContent(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween
         ){
-            Heading2(text = "Register", fontColor = Color.Black)
+            Heading2(text = "Register", fontColor = MaterialTheme.colorScheme.onBackground)
             OnboardingCircleBtn(onClick = {  })
         }
         Column(
@@ -111,7 +90,7 @@ fun SignInContent(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            NormalText(text = "sign up with")
+            NormalText(text = "Sign up with")
             Spacer(modifier = Modifier.height(10.dp))
             SignSocialButtons()
         }
@@ -122,5 +101,7 @@ fun SignInContent(
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 private fun SignUpPrev() {
-    SignInContent(modifier = Modifier.padding(16.dp))
+    PhotoNestTheme {
+        SignUpContent(modifier = Modifier.padding(16.dp))
+    }
 }
