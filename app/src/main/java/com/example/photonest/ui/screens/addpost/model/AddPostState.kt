99206b752e2d54f30670e0648dff2c278f21d202
Ashok <ashok06082@gmail.com>
2024-10-29 02:22:07 +0530

    Add state and event related to the post addition

--- app/src/main/java/com/example/photonest/ui/screens/addpost/model/AddPostState.kt
+++ app/src/main/java/com/example/photonest/ui/screens/addpost/model/AddPostState.kt
@@ -1,4 +1,12 @@
 package com.example.photonest.ui.screens.addpost.model
 
-class AddPostState {
-}
+import android.net.Uri
+
+data class AddPostState(
+    val selectedImageUri: Uri? = null,
+    val selectedCategories: Set<String> = emptySet(),
+    val searchQuery: String = "",
+    val isLoading: Boolean = false,
+    val error: String? = null,
+    val showErrorDialog: Boolean = false
+)
